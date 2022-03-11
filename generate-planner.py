28b3a027d0aafff20c442d5b5cff56fd5e2935a8
John Perret <jperret@umd.edu>
2022-03-10 22:08:42 -0500

    basic planner done

--- generate-planner.py
+++ generate-planner.py
@@ -1,25 +1,107 @@
 from fpdf import FPDF
+import calendar
 
 # https://pyfpdf.github.io/fpdf2/
+# TODO might be able to use margin in FPDF instead of creating it
 
 pdf = FPDF(orientation="l")
+YEAR = 2022
+vertical_padding = 10
+horizontal_padding = 10
+indent_padding = 15
+day_height = (pdf.h-vertical_padding*2)/4
+day_horizontal_spacing = horizontal_padding
+day_width = (pdf.w - horizontal_padding*2 - day_horizontal_spacing) / 2
+rows_per_day = 7
+row_spacing = day_height / (rows_per_day + 1)
+
+# Each month has enough days to fill out every week, when the month ends midweek this means the week is in the ending
+# month and starting month
+# There is probably a better way to get all the dates for a given year
+dates_with_dup = [i for m in range(1,13) for i in calendar.Calendar(firstweekday=0).itermonthdates(YEAR,m)]
+dates = []
+for date in dates_with_dup:
+    if date not in dates:
+        dates.append(date)
+date_iter = iter(dates)
+
 pdf.add_page()
+pdf.set_font('helvetica', 'B', 25)
+pdf.cell(txt="PLANNER" + str(YEAR))
 
-with pdf.new_path() as path:
-    path.style.stroke_width = 1
-    path.move_to(10, 10)
-    path.line_to(100, 10)
-    path.close()
+for first_date in date_iter:
+    pdf.add_page()
+    for y in range(5):
+        if not y == 0:
+            with pdf.new_path() as path:
+                path.style.stroke_width = 0.3
+                path.move_to(horizontal_padding, vertical_padding + day_height*y)
+                path.line_to(horizontal_padding+day_width, vertical_padding + day_height*y)
+                path.close()
 
+        with pdf.new_path() as path:
+            path.style.stroke_width = 0.3
+            path.move_to(horizontal_padding + day_horizontal_spacing + day_width, vertical_padding + day_height*y)
+            path.line_to(pdf.w-horizontal_padding, vertical_padding + day_height*y)
+            path.close()
 
-with pdf.new_path() as path:
-    path.style.stroke_width = 0.25
-    path.move_to(10, 50)
-    path.line_to(200, 50)
-    path.close()
+    for y in range(4):
+        for line in range(1, rows_per_day+1):
+            if not y == 0:
+                with pdf.new_path() as path:
+                    path.style.stroke_width = .15
+                    path.move_to(horizontal_padding +indent_padding, vertical_padding + day_height*y +row_spacing*line)
+                    path.line_to(horizontal_padding+day_width, vertical_padding + day_height*y +row_spacing*line)
+                    path.close()
 
-pdf.set_y(100)
-pdf.set_font('helvetica', 'B', 16)
-pdf.cell(txt='Week of this one!')
+            with pdf.new_path() as path:
+                path.style.stroke_width = .15
+                path.move_to(horizontal_padding +indent_padding + day_width + day_horizontal_spacing, vertical_padding + day_height*y +row_spacing*line)
+                path.line_to(horizontal_padding+day_width+ day_width + day_horizontal_spacing, vertical_padding + day_height*y +row_spacing*line)
+                path.close()
 
-pdf.output("test.pdf")
+    pdf.set_y(10)
+    pdf.set_font('helvetica', 'B', 25)
+    pdf.cell(txt=first_date.strftime("Week Beginning %b %d"))
+
+    pdf.set_font('helvetica', 'B', 25)
+    pdf.set_xy(horizontal_padding, vertical_padding+day_height+2)
+    pdf.cell(w=indent_padding, align="C", txt=str(first_date.day))
+
+    for i in range(2,4):
+        pdf.set_xy(horizontal_padding, vertical_padding+day_height*i+2)
+        pdf.cell(w=indent_padding, align="C", txt=str(next(date_iter).day))
+    '''
+    pdf.set_xy(horizontal_padding, vertical_padding+day_height*3+2)
+    pdf.cell(w=indent_padding, align="C", txt=str(next(date_iter).day))
+'''
+    for i in range(4):
+        pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*i+2)
+        pdf.cell(w=indent_padding, align="C", txt=str(next(date_iter).day))
+    '''
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*1+2)
+    pdf.cell(w=indent_padding, align="C", txt=str(next(date_iter).day))
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*2+2)
+    pdf.cell(w=indent_padding, align="C", txt=str(next(date_iter).day))
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*3+2)
+    pdf.cell(w=indent_padding, align="C", txt=str(next(date_iter).day))
+    '''
+    pdf.set_font('helvetica',"", 15)
+    pdf.set_xy(horizontal_padding, vertical_padding+day_height + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Mon")
+    pdf.set_xy(horizontal_padding, vertical_padding+day_height*2 + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Tues")
+    pdf.set_xy(horizontal_padding, vertical_padding+day_height*3 + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Wed")
+
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Thurs")
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*1 + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Fri")
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*2 + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Sat")
+    pdf.set_xy(horizontal_padding+day_width + day_horizontal_spacing, vertical_padding+day_height*3 + 11)
+    pdf.cell(w=indent_padding, align="C",txt="Sun")
+
+pdf.output(str(YEAR) + "planner.pdf")
+
