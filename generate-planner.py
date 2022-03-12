0eca5be3d476605104329b21706638a0791cf0ff
John Perret <jperret@umd.edu>
2022-03-12 14:49:56 -0500

    Revert "code formatting"
    This reverts commit c8e6eb938affc06ef99530fcbb02846af0b8ba66.

--- generate-planner.py
+++ generate-planner.py
@@ -1,13 +1,14 @@
+from fpdf import FPDF
 import calendar
+import datetime
 
-from fpdf import FPDF  # https://pyfpdf.github.io/fpdf2/
+# https://pyfpdf.github.io/fpdf2/
 
 
 class Iterator:
 	def __init__(self, lst):
 		self.list = lst
 		self.index = 0
-		self.last = None
 
 	def has_next(self):
 		return self.index < len(self.list) - 1
@@ -17,11 +18,7 @@ class Iterator:
 
 	def get_next(self):
 		self.index += 1
-		self.last = self.list[self.index - 1]
-		return self.last
-
-	def get_last(self):
-		return self.last
+		return self.list[self.index -1]
 
 
 pdf = FPDF(orientation="l")
@@ -36,10 +33,6 @@ day_horizontal_spacing = horizontal_padding
 day_width = (pdf.w - horizontal_padding * 2 - day_horizontal_spacing) / 2
 rows_per_day = 7
 row_spacing = day_height / (rows_per_day + 1)
-calendar_width = row_spacing * 6
-calendar_height = row_spacing * 5
-calendar_row_height, calendar_col_width = calendar_height / 7, calendar_width / 7
-calendar_x, calendar_y = pdf.w - horizontal_padding - calendar_width, pdf.h - vertical_padding - calendar_height
 links = dict()
 # Each month has enough days to fill out every week, when the month ends midweek this means the week is in the ending
 # month and starting month
@@ -54,11 +47,10 @@ for date in dates_with_dup:
 pdf.add_page()
 page += 1
 pdf.set_font('helvetica', 'B', 30)
-pdf.set_y(pdf.h / 2)
+pdf.set_y(pdf.h/2)
 pdf.cell(txt=str(YEAR) + " PLANNER", center=True, ln=2)
 pdf.set_font('helvetica', '', 15)
-pdf.cell(txt="https://github.com/jpperret/python-planner", link="https://github.com/jpperret/python-planner",
-		 center=True)
+pdf.cell(txt="https://github.com/jpperret/python-planner", link="https://github.com/jpperret/python-planner", center=True)
 
 date_iter = Iterator(dates)
 
@@ -87,22 +79,23 @@ while date_iter.has_next():
 				with pdf.new_path() as path:
 					path.style.stroke_width = .15
 					path.move_to(horizontal_padding + indent_padding,
-								 vertical_padding + day_height * y + row_spacing * line)
+					             vertical_padding + day_height * y + row_spacing * line)
 					path.line_to(horizontal_padding + day_width, vertical_padding + day_height * y + row_spacing * line)
 					path.close()
 
 			with pdf.new_path() as path:
 				path.style.stroke_width = .15
 				path.move_to(horizontal_padding + indent_padding + day_width + day_horizontal_spacing,
-							 vertical_padding + day_height * y + row_spacing * line)
+				             vertical_padding + day_height * y + row_spacing * line)
 				path.line_to(horizontal_padding + day_width + day_width + day_horizontal_spacing,
-							 vertical_padding + day_height * y + row_spacing * line)
+				             vertical_padding + day_height * y + row_spacing * line)
 				path.close()
 
 	pdf.set_font('helvetica', 'B', 25)
-	pdf.set_xy(0, vertical_padding * 2)
-	pdf.cell(w=horizontal_padding + day_horizontal_spacing + day_width, align="C",
-			 txt=date_iter.peek().strftime("Week Beginning %b %d"))
+	pdf.set_xy(0, vertical_padding*2)
+	pdf.cell(w=horizontal_padding+day_horizontal_spacing+day_width, align="C",
+	         txt=date_iter.peek().strftime("Week Beginning %b %d"))
+	first_date = date_iter.peek()
 
 	for i in range(1, 4):
 		date = date_iter.get_next()
@@ -128,47 +121,49 @@ while date_iter.has_next():
 		pdf.cell(w=indent_padding, align="C", txt=date.strftime("%a"))
 
 	# insert month overview in bottom right corner
+	cw = row_spacing * 6  # calendar width
+	ch = row_spacing * 5  # calendar height
+	crh, ccw = ch / 7, cw / 7  # calendar row height and column width
+	cx, cy = pdf.w-horizontal_padding-cw, pdf.h-vertical_padding - ch  # calendar x and y
+	pdf.set_font('helvetica', "", 11)
 
-	# get date to start calendar - want a monday
-	index_start_calendar = dates.index(date_iter.get_last()) - date_iter.get_last().day + 1
+	# index in dates list of the first day in current month
+	index_start_calendar = dates.index(first_date) - first_date.day + 1
+	# get monday
 	while dates[index_start_calendar].weekday() > 0:
 		index_start_calendar -= 1
 
 	# set grey background
-	pdf.set_xy(calendar_x, calendar_y)
+	pdf.set_xy(cx, cy)
 	pdf.set_fill_color(235)
-	pdf.cell(calendar_width, calendar_height, txt="", fill=True)
+	pdf.cell(cw, ch, txt="", fill=True)
 
 	# Add month header
+	pdf.set_xy(cx, cy)
 	# Pick month based off of index_start_calendar + 2 in case month changes early week
-	pdf.set_font('helvetica', "", 11)
-	pdf.set_xy(calendar_x, calendar_y)
-	pdf.cell(w=calendar_width, txt=dates[index_start_calendar + 2].strftime("%B"), align="C")
+	pdf.cell(w=cw, txt=dates[index_start_calendar + 2].strftime("%B"), align="C")
 
 	# Add weekday headers
-	pdf.set_xy(calendar_x, calendar_y + calendar_row_height)
-	weekdays = ["M", "T", "W", "T", "F", "S", "S"]
+	pdf.set_xy(cx, cy + crh)
+	weekdays = ["M", "T",  "W",  "T", "F", "S", "S"]
 	for c in range(7):
-		pdf.set_xy(calendar_x + calendar_col_width * c, calendar_y + calendar_row_height)
+		pdf.set_xy(cx + ccw*c, cy+crh)
 		pdf.cell(txt=weekdays[c])
 
 	# Add dates and links to page
 	for r in range(5):
 		for c in range(7):
+			pdf.set_xy(cx+ccw*c, cy + crh*(r+2))
+			link = pdf.add_link()
 			page = int((index_start_calendar / 7) + 2)
 			if page < 2:
-				page = 2  # don't link to title page
-			link = pdf.add_link()
+				page = 2
 			pdf.set_link(link, page=page)
-
-			pdf.set_xy(calendar_x + calendar_col_width * c, calendar_y + calendar_row_height * (r + 2))
 			pdf.cell(txt=str(dates[index_start_calendar].day), link=link)
-
 			# Add a border around this week
-			if dates[index_start_calendar] == date_iter.get_last():
-				pdf.set_xy(calendar_x + calendar_col_width * c + calendar_col_width / 10,
-						   calendar_y + calendar_row_height * (r + 2))
-				pdf.cell(w=calendar_width - calendar_col_width / 5, h=calendar_row_height, border=True)
+			if dates[index_start_calendar] == first_date:
+				pdf.set_xy(cx + ccw * c + ccw/10, cy + crh * (r + 2))
+				pdf.cell(w=cw-ccw/5, h=crh, border=True)
 
 			index_start_calendar += 1
 
