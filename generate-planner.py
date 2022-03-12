a858c0087cfa534e3b2a51d51c942808df3e5b46
John Perret <jperret@umd.edu>
2022-03-12 15:23:06 -0500

    code clean up

--- generate-planner.py
+++ generate-planner.py
@@ -1,5 +1,7 @@
-from fpdf import FPDF
 import calendar
+
+from fpdf import FPDF
+
 
 # https://pyfpdf.github.io/fpdf2/
 
@@ -17,7 +19,7 @@ class Iterator:
 
 	def get_next(self):
 		self.index += 1
-		return self.list[self.index -1]
+		return self.list[self.index - 1]
 
 
 pdf = FPDF(orientation="l")
@@ -36,6 +38,7 @@ day_height = (pdf.h - vertical_padding * 2) / 4
 day_width = (pdf.w - horizontal_padding * 2 - day_horizontal_spacing) / 2
 row_spacing = day_height / (rows_per_day + 1)
 links = dict()
+weekdays = ["M", "T", "W", "T", "F", "S", "S"]
 
 # Get all dates in a year
 dates_with_dup = [i for m in range(1, 13) for i in calendar.Calendar(firstweekday=0).itermonthdates(YEAR, m)]
@@ -48,11 +51,11 @@ date_iter = Iterator(dates)
 # Add title page
 pdf.add_page()
 pdf.set_font('helvetica', 'B', 30)
-pdf.set_y(pdf.h/2)
+pdf.set_y(pdf.h / 2)
 pdf.cell(txt=str(YEAR) + " PLANNER", center=True, ln=2)
 pdf.set_font('helvetica', '', 15)
-pdf.cell(txt="https://github.com/jpperret/python-planner", link="https://github.com/jpperret/python-planner", center=True)
-
+pdf.cell(txt="https://github.com/jpperret/python-planner", link="https://github.com/jpperret/python-planner",
+		 center=True)
 
 while date_iter.has_next():
 	pdf.add_page()
@@ -64,43 +67,30 @@ while date_iter.has_next():
 
 	# add week title
 	pdf.set_font('helvetica', 'B', 20)
-	pdf.set_xy(0, vertical_padding*2)
-	pdf.cell(w=horizontal_padding+day_horizontal_spacing+day_width, align="C",
-	         txt=first_date_of_week.strftime("Week Beginning %b %d"))
+	pdf.set_xy(0, vertical_padding * 2)
+	pdf.cell(w=horizontal_padding + day_horizontal_spacing + day_width, align="C",
+			 txt=first_date_of_week.strftime("Week Beginning %b %d"))
 
 	# Add lines to separate days
+	pdf.set_line_width(.3)
 	for y in range(5):
 		if not y == 0:
-			with pdf.new_path() as path:
-				path.style.stroke_width = 0.3
-				path.move_to(horizontal_padding, vertical_padding + day_height * y)
-				path.line_to(horizontal_padding + day_width, vertical_padding + day_height * y)
-				path.close()
-
-		with pdf.new_path() as path:
-			path.style.stroke_width = 0.3
-			path.move_to(horizontal_padding + day_horizontal_spacing + day_width, vertical_padding + day_height * y)
-			path.line_to(pdf.w - horizontal_padding, vertical_padding + day_height * y)
-			path.close()
+			pdf.line(horizontal_padding, vertical_padding + day_height * y, horizontal_padding + day_width,
+					 vertical_padding + day_height * y)
+		pdf.line(horizontal_padding + day_horizontal_spacing + day_width, vertical_padding + day_height * y,
+				 pdf.w - horizontal_padding, vertical_padding + day_height * y)
 
 	# add lines in each day
+	pdf.set_line_width(.12)
 	for y in range(4):
 		for line in range(1, rows_per_day + 1):
 			if not y == 0:
-				with pdf.new_path() as path:
-					path.style.stroke_width = .14
-					path.move_to(horizontal_padding + indent_padding,
-					             vertical_padding + day_height * y + row_spacing * line)
-					path.line_to(horizontal_padding + day_width, vertical_padding + day_height * y + row_spacing * line)
-					path.close()
-
-			with pdf.new_path() as path:
-				path.style.stroke_width = .14
-				path.move_to(horizontal_padding + indent_padding + day_width + day_horizontal_spacing,
-				             vertical_padding + day_height * y + row_spacing * line)
-				path.line_to(horizontal_padding + day_width + day_width + day_horizontal_spacing,
-				             vertical_padding + day_height * y + row_spacing * line)
-				path.close()
+				pdf.line(horizontal_padding + indent_padding, vertical_padding + day_height * y + row_spacing * line,
+						 horizontal_padding + day_width, vertical_padding + day_height * y + row_spacing * line)
+			pdf.line(horizontal_padding + indent_padding + day_width + day_horizontal_spacing,
+					 vertical_padding + day_height * y + row_spacing * line,
+					 horizontal_padding + day_width + day_width + day_horizontal_spacing,
+					 vertical_padding + day_height * y + row_spacing * line)
 
 	# Add date on left
 	for i in range(1, 4):
@@ -131,7 +121,7 @@ while date_iter.has_next():
 	cw = row_spacing * 6  # calendar width
 	ch = row_spacing * 5  # calendar height
 	crh, ccw = ch / 7, cw / 7  # calendar row height and column width
-	cx, cy = pdf.w-horizontal_padding-cw, pdf.h-vertical_padding - ch  # calendar x and y
+	cx, cy = pdf.w - horizontal_padding - cw, pdf.h - vertical_padding - ch  # calendar x and y
 	pdf.set_font('helvetica', "", 11)
 
 	# index in dates list of the first day in current month
@@ -152,25 +142,24 @@ while date_iter.has_next():
 
 	# Add weekday headers
 	pdf.set_xy(cx, cy + crh)
-	weekdays = ["M", "T",  "W",  "T", "F", "S", "S"]
 	for c in range(7):
-		pdf.set_xy(cx + ccw*c, cy+crh)
+		pdf.set_xy(cx + ccw * c, cy + crh)
 		pdf.cell(txt=weekdays[c])
 
 	# Add dates and links to page
 	for r in range(5):
 		for c in range(7):
-			pdf.set_xy(cx+ccw*c, cy + crh*(r+2))
+			pdf.set_xy(cx + ccw * c, cy + crh * (r + 2))
 			link = pdf.add_link()
 			page = int((index_start_calendar / 7) + 2)
 			if page < 2:
-				page = 2
+				page = 2  # don't link to title page
 			pdf.set_link(link, page=page)
 			pdf.cell(txt=str(dates[index_start_calendar].day), link=link)
 			# Add a border around this week
 			if dates[index_start_calendar] == first_date_of_week:
-				pdf.set_xy(cx + ccw * c + ccw/10, cy + crh * (r + 2))
-				pdf.cell(w=cw-ccw/5, h=crh, border=True)
+				pdf.set_xy(cx + ccw * c + ccw / 10, cy + crh * (r + 2))
+				pdf.cell(w=cw - ccw / 5, h=crh, border=True)
 
 			index_start_calendar += 1
 
